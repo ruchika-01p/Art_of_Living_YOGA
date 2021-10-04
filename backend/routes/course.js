8b901e6ec70793dbac5ae1ef34204ddcca287904
ikabir21 <ichankabir@gmail.com>
2021-10-04 22:07:57 +0530

    isAuth middleware added

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -1,9 +1,79 @@
 const router = require("express").Router();
+const  isAuth = require('../middlewares/isAuth');
 const multer = require("multer");
 const Course = require("../models/course.model");
 const upload = require("../middlewares/upload");
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.post("/admin/addCourse", (req, res) => {
+//Create an admin
+// router.post("/admin/register", (req, res) => {
+// 	const { username, password, password2 } = req.body;
+// 	if (!username || !password || !password2)
+// 		return res.status(400).json({ success: false, message: "All the fields are required" });
+
+// 	if (password !== password2) 
+// 		return res.status(400).json({ success: false, message: "Passwords do not match" });
+
+// 	User.find({}).then((user) => {
+// 			if (user && user.length > 0) 
+// 				return res.status(400).json({ success: false, message: "Admin already exists" });
+
+// 			bcrypt.genSalt(10, function (err, salt) {
+// 				if (err)
+// 					return res.status(400).json({ success: false, message: err.message });
+
+// 				bcrypt.hash(password, salt, function (err, hashedPassword) {
+// 					if (err)
+// 						return res.status(400).json({ success: false, message: err.message });
+
+// 					User.create({ username, password: hashedPassword }, function (err, doc) {
+// 						if (err)
+// 							res.status(400).json({ success: false, message: err.message });
+// 						return res.status(201).json({ success: true });
+// 					});
+// 				});
+// 			});
+// 		})
+// 		.catch((err) =>
+// 			res.status(400).json({ success: false, message: err.message })
+// 		);
+// });
+
+router.post("/admin/login", (req, res) => {
+	console.log(req.user);
+	const { username, password } = req.body
+	;
+	User.findOne({ username })
+		.then((user) => {
+			if (!user)
+				return res
+					.status(404)
+					.json({ success: false, message: "User not found" });
+
+			bcrypt.compare(password, user.password, (err, isValidPassword) => {
+				if (err) res.status(400).json({ success: false, message: err.message });
+				if (!isValidPassword)
+					res
+						.status(400)
+						.json({ success: false, message: "Invalid credentials" });
+				const token = jwt.sign(
+					{ username: user.username },
+					process.env === "production" ? process.env.JWT_SECRERT : "123",
+					{ expiresIn: "15d" }
+				);
+
+				return res.json({ success: true, user: token });
+			});
+		})
+		.catch((err) =>
+			res.status(400).json({ success: false, message: err.message })
+		);
+});
+
+router.post("/admin/addCourse", isAuth, (req, res) => {
+	console.log(req)
 	upload(req, res, (err) => {
 		if (err instanceof multer.MulterError) {
 			return res.status(500).json(err);
